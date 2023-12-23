c7501eff45a66ac5d5688987e5333afd0fdfae28
asrorxoja07 <asrorxojaisomiddinov21@gmail.com>
2023-12-23 22:30:07 +0500

    masalalar

---
+++ index.js
@@ -0,0 +1,78 @@
+// 1-masala
+
+
+// let arr = [5, 3, 344, 4, 7];
+
+// function find(arr,res){
+//     let result = arr.indexOf(res)
+//     if (result){
+//     return result ** 2;
+//    }else{
+//     return "mavjud emas";
+//    }
+   
+// };
+
+// console.log(find(arr,4));
+
+
+
+
+
+// 2-masala
+
+// let massiv1 =[1, 2, 3];
+// let massiv2 =[4, 5, 6];
+// let massiv3 =[7, 8, 9];
+//  function massiv(arg){
+//     arr = massiv2.concat(massiv1, massiv3);
+//  }
+ 
+
+// massiv();
+//  console.log(arr);
+
+
+
+
+// 3-masala
+
+// let arr = [1,2,3,4,5,6];
+
+// function massiv(arg){
+//     let a = arr.find((value, index, arr) => {
+//         return value > 4 ;
+        
+//        })
+//        console.log(a ** 2);
+// }
+// massiv();
+
+
+// 4-masala
+
+
+// let arr = [1,2,3,4,5,6,7,8,9,10];
+// let counter=0;
+
+// arr.forEach((value, index, arr) => {
+    
+//     if(arr[index] % 2 ==1){
+//         counter++;
+       
+//     }
+  
+//     return counter;
+
+// })
+// console.log(counter);
+
+
+// 5-masala
+
+// let massiv = ['salom', 'dunyo', 'hello','world'];
+// massiv.forEach(function(soz) {
+//     let lastletter = soz.charAt(soz.length - 1);
+//     console.log(lastletter);
+// });
+
